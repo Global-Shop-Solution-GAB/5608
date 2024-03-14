b13f8ab85fefd56bbbdbeba4ca146b00135d9d0f
Service Web <sw@gssmail.com>
2024-03-14 12:27:49 -0500

    Commit changes in C:\Websites\Git\emarlow\5608\GCG_5608_TABLE_FIELD.sql

---
+++ GCG_5608_TABLE_FIELD.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5608_TABLE_FIELD
+ * ========================================== */
+CREATE TABLE "GCG_5608_TABLE_FIELD"(
+ "PROFILE" CHAR(10) NOT NULL ,
+ "FIELD_NAME" CHAR(20),
+ "FIELD_INDEX" NUMERIC(8,0),
+ "TABLE_INDEX" NUMERIC(8,0));
+
+
