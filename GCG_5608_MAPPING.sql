be2bf90c712c094a80bea7217c9ac431d310b8fa
Service Web <sw@gssmail.com>
2024-03-14 12:27:49 -0500

    Commit changes in C:\Websites\Git\emarlow\5608\GCG_5608_MAPPING.sql

---
+++ GCG_5608_MAPPING.sql
@@ -0,0 +1,12 @@
+/* ==========================================
+ * TABLE: GCG_5608_MAPPING
+ * ========================================== */
+CREATE TABLE "GCG_5608_MAPPING"(
+ "PROFILE" CHAR(10) NOT NULL ,
+ "FILE_TITLE" CHAR(30),
+ "FILE_INDEX" NUMERIC(8,0),
+ "GSS_NAME" NCHAR(20),
+ "FIELD_INDEX" CHAR(6),
+ "TABLE_INDEX" NUMERIC(8,0));
+
+
