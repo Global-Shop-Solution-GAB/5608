4ac5ee5eb98ada776c7e32ef84629b29fb3ab43f
Service Web <sw@gssmail.com>
2024-03-14 12:27:49 -0500

    Commit changes in C:\Websites\Git\emarlow\5608\GCG_5608_TABLE_MAP.sql

---
+++ GCG_5608_TABLE_MAP.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5608_TABLE_MAP
+ * ========================================== */
+CREATE TABLE "GCG_5608_TABLE_MAP"(
+ "PROFILE" CHAR(10) NOT NULL ,
+ "TABLE_NAME" CHAR(20),
+ "TABLE_INDEX" NUMERIC(8,2),
+ "TABLE_KEY" CHAR(30));
+
+
