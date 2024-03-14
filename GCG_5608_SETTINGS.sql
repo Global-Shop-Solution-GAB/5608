39cbeb178b31296b36a2bb5ac986dfd91fec7501
Service Web <sw@gssmail.com>
2024-03-14 12:27:49 -0500

    Commit changes in C:\Websites\Git\emarlow\5608\GCG_5608_SETTINGS.sql

---
+++ GCG_5608_SETTINGS.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5608_SETTINGS
+ * ========================================== */
+CREATE TABLE "GCG_5608_SETTINGS"(
+ "PROFILE" CHAR(10) NOT NULL ,
+ "TYPE" CHAR(20),
+ "SETTING" CHAR(20),
+ "VALUE" LONGVARCHAR);
+
+
